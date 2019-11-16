fa5a45f25a47c2f404414bc8e81685e2f1be9206
JericHunter <jeric.hunterr@gmail.com>
2019-11-15 22:52:03 -0800

    done

---
+++ wiki/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from wiki.models import Page
+from django.views.generic import DetailView, ListView
+from django.http import Http404, HttpResponse
+
+# Create your views here.
+
+
+class PageList(ListView):
+    """
+    CHALLENGES:
+      1. On GET, display a homepage that shows all Pages in your wiki.
+      2. Replace this CHALLENGE text with a descriptive docstring for PageList.
+      3. Replace pass below with the code to render a template named `list.html`.
+    """
+    model = Page
+
+    def get(self, request):
+      """ Returns a list of wiki pages. """
+      wiki_list = Page.objects.all()
+      context = {'wiki_list': wiki_list}
+      return render(request, 'list.html', context=context)
+
+class PageDetailView(DetailView):
+    """
+    CHALLENGES:
+      1. On GET, render a template named `page.html`.
+      2. Replace this docstring with a description of what thos accomplishes.
+
+    STRETCH CHALLENGES:
+      1. Import the PageForm class from forms.py.
+          - This ModelForm enables editing of an existing Page object in the database.
+      2. On GET, render an edit form below the page details.
+      3. On POST, check if the data in the form is valid.
+        - If True, save the data, and redirect back to the DetailsView.
+        - If False, display all the errors in the template, above the form fields.
+      4. Instead of hard-coding the path to redirect to, use the `reverse` function to return the path.
+      5. After successfully editing a Page, use Django Messages to "flash" the user a success message
+           - Message Content: REPLACE_WITH_PAGE_TITLE has been successfully updated.
+    """
+    model = Page
+
+    def get(self, request, slug):
+        """ Returns a specific of wiki page by slug. """
+        try:
+          wiki = Page.objects.get(slug=slug)
+        except Page.DoesNotExist:
+          raise Http404("Page does not exist")
+        return render(request, 'page.html', {'wiki': wiki})
